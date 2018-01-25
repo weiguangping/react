426fb70ce01af1225fac36c79d89d52ef9fe18d0
weiguangping <757846526@qq.com>
2018-01-25 23:10:59 +0800

    1

---
+++ src/js/components/pc_index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PCHeader from './pc_header'
+import PCFooter from './pc_footer'
+// import PCFooter from './pc_footer'
+
+export default class PCIndex extends React.Component {
+  render () {
+    return (
+      <div>
+        <PCHeader />
+        <PCFooter />
+      </div>
+    )
+  };
+}
--- src/js/root.js
+++ src/js/root.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Router, Route, hashHistory} from 'react-router'
-
+// import {Router, Route, hashHistory} from 'react-router'
+import {Button} from 'antd'
+import 'antd/dist/antd.css'
+import PCIndex from './components/pc_index'
+import MBIndex from './components/mobile_index'
+import MediaQuery from 'react-responsive'
 export default class Root extends React.Component {
   render () {
     return (
-      <div>111</div>
+      <div>
+        <MediaQuery query='(min-device-width: 1224px)' >
+          <PCIndex />
+        </MediaQuery>
+        <MediaQuery query='(max-device-width: 1224px)'>
+          <MBIndex />
+        </MediaQuery>
+      </div>
     )
   };
 }
 
 ReactDOM.render(
-  <Root />, document.getElementById('example'))
+  <Root />,
+document.getElementById('mainContainer'))
