d75de8a6845900aca8f4c86a5be0e42f1592f1ba
“tuimeo_weiguangping <weiguangping@tuimeo.cn>
2018-01-26 14:19:01 +0800

    1

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,27 @@
-var debug = process.env.NODE_ENV !== 'production'
-var webpack = require('webpack')
 var path = require('path')
-
 module.exports = {
-  context: path.join(__dirname),
-  devtool: debug ? 'inline-sourcemap' : null,
+    // 入口文件
   entry: './src/js/root.js',
-  module: {
-    loaders: [
-      {
-        test: /\.js?$/,
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015'],
-          plugins: ['react-html-attrs'] // 添加组件的插件配置
-        }
-      },
-      // 下面是使用 ant-design 的配置文件
-      { test: /\.css$/, loader: 'style-loader!css-loader' }
-    ]
-  },
+    // 出口文件
   output: {
-    path: __dirname,
-    filename: './src/bundle.js'
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
-  ]
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      exclude: /(node_modules)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015'],
+        plugins: ['react-html-attrs'] // 添加组件的插件配置
+      }
+    },
+            // 下面是使用 ant-design 的配置文件
+    {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    }
+    ]
+  }
 }
